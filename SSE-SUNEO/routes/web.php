76a83ddffe771aa489d6f0bc7bebf0f5b5806240
Dave <davedelcastillo14@gmail.com>
2022-10-20 13:24:54 -0500

    Cambios en las vistas

--- SSE-SUNEO/routes/web.php
+++ SSE-SUNEO/routes/web.php
@@ -10,6 +10,7 @@ Route::prefix('Empresas')->group(function () {
     Route::get('/propuesta-suneo', 'RegistroEmpresaController@view_propuesta_suneo')->name('propuesta-suneo');
     Route::get('/perfil-empresa', 'RegistroEmpresaController@view_perfil_empresa')->name('perfil-empresa');
     Route::get('/encuesta-de-satisfaccion', 'RegistroEmpresaController@view_encuesta_satisfaccion');
+    
     Route::post('/create', 'RegistroEmpresaController@create')->name('Empresa.create');
     Route::post('/stored_encuesta', 'RegistroEmpresaController@stored_encuesta')->name('Empresa.stored_encuesta');
 });
